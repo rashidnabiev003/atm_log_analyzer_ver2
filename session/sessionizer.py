6c8ecb4ee3aadcad6d4bd73465a308290011a370
rashidnabiev003 <rashidkhonnabiev@gmail.com>
2026-04-28 14:15:52 +0500

    Fix: sessionizer problem

--- session/sessionizer.py
+++ session/sessionizer.py
@@ -1,18 +1,24 @@
+import re
 from typing import Iterable, List, Iterator
 
-MAIN_PAGE_MARKER = 'Entered `main.html` page.' 
+
+SESSION_ANCHOR_RE = re.compile(r"Entered\s+[`'\"“”‘’]?\s*main\.html\s*[`'\"“”‘’]?\s+page\.?",re.IGNORECASE)
+
+
+def is_session_anchor(line: str) -> bool:
+    return SESSION_ANCHOR_RE.search(line) is not None
 
 
 def split_sessions(lines: Iterable[str]) -> Iterator[List[str]]:
-    """
-    Функция генератор, которая разделяет сессии пользования терминалом
-    """
     current_session: List[str] = []
+
     for line in lines:
-        if MAIN_PAGE_MARKER in line:
+        if is_session_anchor(line):
             if current_session:
                 yield current_session
                 current_session = []
+
         current_session.append(line)
+
     if current_session:
         yield current_session
